a7fe6e3c787c5b8b1a1b0b92d103f1b5ee3bb2fc
TharunN <nuthikattu.tharun6@gmail.com>
2025-07-12 14:04:47 +0530

    NewsAggregatorAPIwithSpringBoot_Tharun

---
+++ src/main/java/com/example/NewsAggregatorAPIwithSpringBoot/NewsAggregatorApIwithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.NewsAggregatorAPIwithSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsAggregatorApIwithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsAggregatorApIwithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NewsAggregatorAPIwithSpringBoot/NewsAggregatorApIwithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NewsAggregatorAPIwithSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsAggregatorApIwithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
